09656a0c0126b6990c5935d4ed048375c01c4b56
Veldoxx <chitzthemonkey@gmail.com>
2025-01-24 04:33:40 -0500

    Script Update for scrolling
    Brandon actually wrote this code

--- Scripts/ScrollContainer.gd
+++ Scripts/ScrollContainer.gd
@@ -2,6 +2,7 @@ extends ScrollContainer
 @onready var scroll_up = $"../../Scroll Up"
 @onready var scroll_down = $"../../Scroll Down"
 @onready var game_manager = $"../../../../GameManager"
+@onready var i_slots = $InventorySlots
 
 
 
@@ -12,8 +13,10 @@ var scale_shrink = 0.4
 var scale_default = 0.5
 const OBJECTSCENE = preload("res://Scenes/object.tscn")
 var hovering = false
+var max_scroll = 0
 
-
+func _ready():
+	max_scroll = i_slots.size.y - size.y
 			
 func _input(event):
 	
@@ -64,7 +67,7 @@ func _process(delta):
 		scroll_up.visible = false
 	else:
 		scroll_up.visible = true
-	if get_v_scroll() == 3183:
+	if get_v_scroll() >= max_scroll-1:
 		scroll_down.visible = false
 	else:
 		scroll_down.visible = true
